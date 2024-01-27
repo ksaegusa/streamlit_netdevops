6427418095a7ab93c9e20f21ee325d9acd2a6484
ksaegusa <snack11monster@gmail.com>
2024-01-27 23:34:54 +0900

    fix

--- home.py
+++ home.py
@@ -1,7 +1,99 @@
+
+import re 
+import copy
+import glob
+from io import StringIO
+
+import jmespath
+import pandas as pd
 import streamlit as st
+import textfsm
 
 st.title("NetDevOpsSupporter")
-if st.button("Parser"):
-    st.switch_page("pages/parse.py")
-if st.button("Regex"):
-    st.switch_page("pages/regex.py")
+
+parser, regex = st.tabs(["Parser", "Regex"])
+
+with parser:
+    st.title("Parser")
+
+    parsed_list = list()
+    template_root_dir = "files/templates/"
+    template_dir_full_path = glob.glob(f"{template_root_dir}*.textfsm")
+    template_dir = list(map(lambda x: x.replace(template_root_dir,""),template_dir_full_path))
+
+    log_upload = st.toggle("ログアップロード")
+    if log_upload:
+        log_file = st.file_uploader("ログファイル")
+    else:
+        log_file = st.text_area("テキスト貼り付け", key="parser_textarea")
+
+    template_upload = st.toggle("テンプレートアップロード")
+    if template_upload:
+        parser_file = st.file_uploader("パーサー")
+    else:
+        parser_file = st.selectbox("パーサー", template_dir)
+
+    if log_file and parser_file:
+        if log_upload:
+            text = log_file.getvalue().decode("utf-8")
+        else:
+            text = log_file
+        if template_upload:
+            template = StringIO(parser_file.getvalue().decode("utf-8"))
+        else:
+            with open(template_root_dir + parser_file, "r", encoding="UTF-8") as f:
+                template = StringIO(f.read())
+        fsm = textfsm.TextFSM(template)
+        header = fsm.header
+        parsed_data = fsm.ParseText(text)
+
+        for i in parsed_data:
+            obj = {}
+            for n,m in zip(i,header):
+                obj[m] = n
+            parsed_list.append(obj)
+
+        df = pd.DataFrame(parsed_list)
+        st.data_editor(
+            df,
+            use_container_width=True,
+            hide_index=True,
+            num_rows="dynamic"
+        )
+    else:
+        st.write("ファイルがありません")
+
+    query  =st.text_input("クエリ文", "[?PROTOCOL == `C`]")
+    if st.button("クエリ実行"):
+        query_list = copy.deepcopy(parsed_list)
+        queried_list = jmespath.search(query, query_list)
+        queried_df = pd.DataFrame(queried_list)
+        st.write(queried_df)
+
+with regex:
+    st.title("Regex")
+
+    re_log_file = ""
+    re_log_upload = st.toggle("ログアップデート")
+
+    if re_log_upload:
+        re_log_file = st.file_uploader("ログアップロード")
+    else:
+        re_text = st.text_area("テキスト貼り付け", key="res_textarea")
+
+    if re_log_file:
+        re_text = re_log_file.getvalue().decode("utf-8")
+        container = st.container(border=True, height=400)
+        container.code(re_text, language="log")
+
+    regex_string = st.text_input("正規表現", "\s+")
+    if st.button("抽出"):
+        lines = re_text.splitlines()
+        res_lines = list()
+        for line in lines:
+            if re.match(rf"{regex_string}", line):
+                res_lines.append(line)
+        st.write(f"Condition: {regex_string}")
+        st.write(f"Result   : Hit {len(res_lines)} lines")
+        res_container = st.container(border=True, height=250)
+        res_container.code("\n".join(res_lines), language="log")
