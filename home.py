e0f16b20b9f21eb44e51f2f8ba8ead23a0d84b0c
ksaegusa <snack11monster@gmail.com>
2024-01-28 11:14:01 +0900

    add commander

--- home.py
+++ home.py
@@ -8,10 +8,10 @@ import jmespath
 import pandas as pd
 import streamlit as st
 import textfsm
+from netmiko import ConnectHandler
 
 st.title("NetDevOpsSupporter")
-
-parser, regex = st.tabs(["Parser", "Regex"])
+parser, regex, commander = st.tabs(["Parser", "Regex","Commander"])
 
 with parser:
     st.title("Parser")
@@ -97,3 +97,51 @@ with regex:
         st.write(f"Result   : Hit {len(res_lines)} lines")
         res_container = st.container(border=True, height=250)
         res_container.code("\n".join(res_lines), language="log")
+
+with commander:
+    st.title("Commander")
+    # NOTE: 適当にピックアップ
+    #       Netmikoのプラットフォーム対応は下記を参照
+    #       https://ktbyers.github.io/netmiko/PLATFORMS.html
+    device_type_list =[
+        "cisco_ios",
+        "cisco_asa",
+        "cisco_nxos",
+        "cisco_xe",
+        "cisco_xr",
+        "linux",
+        "vyos",
+        "yamaha",
+    ]
+
+    with st.expander("認証情報"):
+        address = st.text_input("アドレス","X.X.X.X")
+        user = st.text_input("ユーザー","admin")
+        password = st.text_input("パスワード", "password")
+        secret = st.text_input("enableパスワード", "")
+        device_type = st.selectbox("デバイスタイプ", device_type_list)
+
+    command = st.text_input("コマンド","show ip route")
+
+    device = {
+        "device_type": device_type,
+        "host": address,
+        "username": user,
+        "password": password,
+        "secret": secret
+    }
+
+    if st.button("実行"):
+        with ConnectHandler(**device) as net_connect:
+            output = net_connect.send_command(command)
+
+        if secret:
+            net_connect.enable()
+
+        commander_container = st.container(border=True, height=700)
+        commander_container.code(output, language="log")
+        st.download_button(
+            label="テキストダウンロード",
+            data=output,
+            file_name=f"{address}_{command.replace(' ','_')}.text"
+        )
